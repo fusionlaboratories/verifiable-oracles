5cb6bbc4bc9bd6ead1543b3591e740f10fcf0651
Qredo LTD <support@qredo.com>
2023-07-26 11:53:02 +0200

    Add devenv dev environment with golang and ignite
    - adds ignite nix wrapper in ./nix/ignite.nix (darwin_arm64),
    - updates README.md with directions how to use the dev environment,
    - bootstraps devenv developer environment with golang.

---
+++ nix/ignite.nix
@@ -0,0 +1,18 @@
+# Ignite nix wrapper
+{ pkgs }:
+
+let
+version = "0.27.1";
+sha256sums = {
+  "0.27.1" = "N28aJdE0sfeo/DHVj0GF/KUKge+/1X4EHUNfsu4u5u0=";
+};
+tarball = pkgs.fetchurl {
+  url = "https://github.com/ignite/cli/releases/download/v${version}/ignite_${version}_darwin_arm64.tar.gz";
+  sha256 = sha256sums."${version}";
+};
+in
+pkgs.runCommand "ignite-${version}" {} ''
+mkdir -p $out/bin
+tar xf ${tarball}
+mv ignite $out/bin/
+''
