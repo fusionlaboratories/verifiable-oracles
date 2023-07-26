00f6e66bb1141da43c88ab38ad3097390e3dd9a8
Qredo LTD <support@qredo.com>
2023-07-26 13:04:22 +0200

    Add CI test
    - adds a separate CI job that tests the dev shell,
    - adds support for ignite running on linux.

--- nix/ignite.nix
+++ nix/ignite.nix
@@ -1,12 +1,24 @@
-# Ignite nix wrapper
-{pkgs}: let
+# Ignite nix wrapper, currently only supports arm64
+{
+  pkgs,
+  stdenv,
+}: let
   version = "0.27.1";
+  system =
+    if stdenv.isDarwin
+    then "darwin"
+    else "linux";
   sha256sums = {
-    "0.27.1" = "N28aJdE0sfeo/DHVj0GF/KUKge+/1X4EHUNfsu4u5u0=";
+    darwin = {
+      "0.27.1" = "N28aJdE0sfeo/DHVj0GF/KUKge+/1X4EHUNfsu4u5u0=";
+    };
+    linux = {
+      "0.27.1" = "/kujtVVpraAtZMJU/pWb9pQG6Bz6/1sTq1Yz6wxgckU=";
+    };
   };
   tarball = pkgs.fetchurl {
-    url = "https://github.com/ignite/cli/releases/download/v${version}/ignite_${version}_darwin_arm64.tar.gz";
-    sha256 = sha256sums."${version}";
+    url = "https://github.com/ignite/cli/releases/download/v${version}/ignite_${version}_${system}_arm64.tar.gz";
+    sha256 = sha256sums."${system}"."${version}";
   };
 in
   pkgs.runCommand "ignite-${version}" {} ''
