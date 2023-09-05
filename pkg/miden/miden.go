e44721150c301ba54284aaccbcaaf8f3f6e49ee1
Qredo LTD <support@qredo.com>
2023-09-05 12:06:24 +0200

    Add a call to check Miden version

--- pkg/miden/miden.go
+++ pkg/miden/miden.go
@@ -1,1 +1,15 @@
 package miden
+
+import (
+	"os/exec"
+	"strings"
+)
+
+// Execute Miden and get it's version
+func Version() (string, error) {
+	out, err := exec.Command("miden", "--version").Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(out)), nil
+}
--- pkg/miden/miden_test.go
+++ pkg/miden/miden_test.go
@@ -3,6 +3,10 @@ package miden_test
 import (
 	"os/exec"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/qredo/verifiable-oracles/pkg/miden"
 )
 
 var testHasMiden bool
@@ -17,4 +21,16 @@ func TestMiden(t *testing.T) {
 	if !testHasMiden {
 		t.Skip("miden not found, skipping")
 	}
+}
+
+func TestMidenVersion(t *testing.T) {
+	if !testHasMiden {
+		t.Skip("miden not found, skipping")
+	}
+
+	assert := assert.New(t)
+
+	v, err := miden.Version()
+	assert.Nil(err)
+	assert.Equal("Miden 0.6.0", v)
 }
